6458b9030900338a6aa480991e7c1bc3573bbb9f
Fred <64082311+MatiP911@users.noreply.github.com>
2024-12-04 14:49:28 +0100

    Fisrt commmit with working setup.

---
+++ after/plugin/autocomment.lua
@@ -0,0 +1,7 @@
+require('nvim_comment').setup({line_mapping = "<leader>c", operator_mapping = "<C-_>", comment_chunk_text_object = "ic",
+-- z jakiegoś powodu <C-_> oznaczca ctrl+/
+      hook = function()
+    if vim.api.nvim_buf_get_option(0, "filetype") == "cpp" or vim.api.nvim_buf_get_option(0, "filetype") == "c" then
+        vim.api.nvim_buf_set_option(0, "commentstring", "//%s")
+    end
+  end})
