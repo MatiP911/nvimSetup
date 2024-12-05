fec556cc7c5b43c6f8aca5f5c97bc0f1bc65135d
Fred <64082311+MatiP911@users.noreply.github.com>
2024-12-05 14:34:18 +0100

    autocomment & harpoon done

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,7 +4,6 @@ return {
     {'hrsh7th/nvim-cmp'},
     {"polirritmico/monokai-nightasty.nvim", lazy = false, priority = 1000},
     {'mg979/vim-visual-multi'},
-    {'m4xshen/autoclose.nvim'},
     {'terrortylor/nvim-comment'},
     {'ggandor/leap.nvim'}
     --   { import = "plugins" },
--- lua/plugins/autocomment.lua
+++ lua/plugins/autocomment.lua
@@ -1,7 +1,11 @@
-require('nvim_comment').setup({line_mapping = "<leader>c", operator_mapping = "<C-_>", comment_chunk_text_object = "ic",
--- z jakiegoś powodu <C-_> oznaczca ctrl+/
-      hook = function()
-    if vim.api.nvim_buf_get_option(0, "filetype") == "cpp" or vim.api.nvim_buf_get_option(0, "filetype") == "c" then
-        vim.api.nvim_buf_set_option(0, "commentstring", "//%s")
-    end
-  end})
+return {
+    'm4xshen/autoclose.nvim',
+    config = function()
+        require('nvim_comment').setup({line_mapping = "<leader>c", operator_mapping = "<C-_>", comment_chunk_text_object = "ic",
+        -- z jakiegoś powodu <C-_> oznaczca ctrl+/
+        hook = function()
+            if vim.api.nvim_buf_get_option(0, "filetype") == "cpp" or vim.api.nvim_buf_get_option(0, "filetype") == "c" then
+                vim.api.nvim_buf_set_option(0, "commentstring", "//%s")
+            end
+        end})
+    end}
