9b1ed0c217b147338c9d9f4876ce8110fd3b4f46
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-20 14:59:59 +0200

    added animate

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -1,7 +1,14 @@
 return {
     'echasnovski/mini.nvim',
     config = function()
-        require('mini.diff').setup() -- Git diff
-        require('mini.ai').setup() -- Better surround
+        require('mini.diff').setup()    -- Git diff
+        require('mini.ai').setup()      -- Better surround
+        require('mini.animate').setup({ -- Animations
+            cursor = { enable = false },
+            scroll = {
+                enable = true,
+                timing = function() return 1 end,
+            },
+        })
     end
 }
